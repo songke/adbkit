e099a14a97bc45c085b0e0dbec6acdb3649de1b1
songke <songke@weidian.com>
2019-10-22 22:05:18 +0800

    update js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = require('./src/adb')
+(function() {
+  var Path;
+
+  Path = require('path');
+
+  module.exports = (function() {
+    switch (Path.extname(__filename)) {
+      case '.coffee':
+        return require('./src/adb');
+      default:
+        return require('./lib/adb');
+    }
+  })();
+
+}).call(this);
